a1ea6870eb6d63deae5a7e4c1a4b6aff1f987e26
GarrikDanilov <garrik_danilov@mail.ru>
2023-03-05 23:26:19 +0300

    lesson_4

--- blog/app.py
+++ blog/app.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask, render_template
+from flask_migrate import Migrate
 from blog.views.users import users_app
 from blog.views.articles import articles_app
 from blog.models.database import db
@@ -6,14 +8,15 @@ from blog.views.auth import login_manager, auth_app
 
 
 app = Flask(__name__)
+
+migrate = Migrate(app, db, compare_type=True)
+
 app.register_blueprint(users_app, url_prefix='/users')
 app.register_blueprint(articles_app, url_prefix='/articles')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/blog.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+cfg_name = os.environ.get('CONFIG_NAME') or 'ProductionConfig'
+app.config.from_object(f'blog.configs.{cfg_name}')
 db.init_app(app)
-
-app.config['SECRET_KEY'] = 'abcdefg123456'
 
 app.register_blueprint(auth_app, url_prefix='/auth')
 
